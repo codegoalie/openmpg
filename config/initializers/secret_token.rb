b121746b2144a3a54df736721c18ba6cbbe77ee7
Chris Marshall <chris@chrismar035.com>
2012-06-21 23:36:01 -0400

    Initial Commit; fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenMPG::Application.config.secret_token = '7fe3ac48a095c7a2fa33d22088e1b7f712a6cf926a336b0fa27c61ce0de2732e07295364160c4f3b985749cb549f916208b61057725bee3df0929d9cd5fb510f'
