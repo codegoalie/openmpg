0d9d4b09f57d90a1f417ea905c107febb67a11e1
Chris Marshall <chris@chrismar035.com>
2012-11-13 20:48:20 -0500

    Flesh out fillup observer

--- app/models/fillup_observer.rb
+++ app/models/fillup_observer.rb
@@ -1,9 +1,23 @@
 class FillupObserver < ActiveRecord::Observer
 
   def after_save(fill_up)
+    calc_next(fill_up)
+    calc_prev(fill_up)
+  end
+
+  private
+
+    def calc_next(fill_up)
       if fill_up.mileage_changed? && next_fillup = fill_up.next_fillup
         next_fillup.calculate_mpg
-        next_fillup.save
+        next_fillup.save!
       end
-  end
+    end
+
+    def calc_prev(fill_up)
+      if fill_up.mileage_changed? && prev_fillup = fill_up.previous_fillup
+        prev_fillup.calculate_mpg
+        prev_fillup.save!
+      end
+    end
 end
